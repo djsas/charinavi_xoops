1fb84f126436220b4193351a400e3f65c0969ccd
Satoru Sasaki <satoru.sasaki@charity-japan.com>
2011-09-13 00:57:53 +0900

    noneモジュールをインポート。

---
+++ xoops_version.php
@@ -0,0 +1,26 @@
+<?php
+// $Id: xoops_version.php,v 1.4 2003/09/21 07:40:10 kousuke Exp $
+
+// change 
+$modversion['name'] = 'チャリナビ by Charity Japan';
+$modversion['dirname'] = "charinavi";
+
+
+
+$modversion['description'] = $modversion['name'];
+$modversion['version'] = "0.1";
+$modversion['credits'] = "";
+$modversion['author'] = 'djsas';
+$modversion['help'] = "help.html";
+$modversion['license'] = "GPL see LICENSE";
+$modversion['official'] = 0;
+$modversion['image'] = "logo.gif";
+
+//Admin things
+$modversion['hasAdmin'] = 0;
+$modversion['hasMain'] = 1;
+
+
+
+
+?>
